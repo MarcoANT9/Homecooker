4c6487f876a4426a73bc4531cfaa1d764dc77b10
noah172 <jsebastian.garcia1995@gmail.com>
2020-06-08 15:53:18 -0500

    deleted 'import sqlalchemy'

--- models/recipe.py
+++ models/recipe.py
@@ -4,8 +4,8 @@
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String, Foregnkey, Table, Integer
+from sqlalchemy.orm import relationship
 
 
 class Recipe(BaseModel, Base):
