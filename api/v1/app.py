0f87c6195f5715f109c5362517ea5a65cdf4a414
noah172 <jsebastian.garcia1995@gmail.com>
2020-06-09 10:47:36 -0500

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,3 +9,27 @@ from flasgger import Swagger
 from flasgger.utils import swag_from
 
 app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix='/api/v1')
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Close Storage """
+    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+if __name__ == '__main__':
+    if getenv('HMCR_API_HOST'):
+        app.run(host=getenv('HMCR_API_HOST'),
+                port=getenv('HMCR_API_PORT'), threaded=True)
+    else:
+        app.run(host='0.0.0.0', threaded=True)  
