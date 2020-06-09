764c8a9e6fa31899ad568dc77521f61407911660
noah172 <jsebastian.garcia1995@gmail.com>
2020-06-09 10:48:58 -0500

    added status

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,3 +7,4 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.user import *
 from api.v1.views.recipe import *
 from api.v1.views.review import *
+from api.v1.views.status import *
