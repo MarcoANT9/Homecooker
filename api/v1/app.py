0e52a176b1d5aeeaffc1e63151de6b03d58017f1
noah172 <jsebastian.garcia1995@gmail.com>
2020-06-09 16:46:21 -0500

    imported getenv

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,11 +2,9 @@
 """ Flask Application """
 from models import storage
 from api.v1.views import app_views
-from os import environ
+from os import environ, getenv
 from flask import Flask, render_template, make_response, jsonify
 from flask_cors import CORS
-from flasgger import Swagger
-from flasgger.utils import swag_from
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
