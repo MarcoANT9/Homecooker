2ddd13dd56db2227223d5602b0783ff6e4ef73bc
MarcoANT9 <acalde27@hotmail.com>
2020-06-13 19:03:18 +0000

    models-init: Changed envar storage name

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,7 @@ Initialices the package
 """
 from os import getenv
 
-stora_type = getenv("HOME_TYPE_STORAGE")
+stora_type = getenv("HMCR_TYPE_STORAGE")
 
 if stora_type == "db":
     from models.engine.dbStorage import DbStorage
