63ea4478833e24749ba64b3f771e2db77ff4387f
MarcoANT9 <acalde27@hotmail.com>
2020-06-24 19:03:33 +0000

    Web_flask-Index.py: Style revision

--- web_flask/index.py
+++ web_flask/index.py
@@ -1,11 +1,12 @@
 import json
+from flask import Flask, request, render_template, redirect, url_for, session,
+from flask import jsonify
+from flask_mysqldb import MySQL
 from models import storage
 from models.user import User
 from models.recipe import Recipe
 from models.review import Review
 from os import environ, getenv
-from flask import Flask, request, render_template, redirect, url_for, session, jsonify
-from flask_mysqldb import MySQL
 import requests
 
 app = Flask(__name__)
@@ -17,6 +18,8 @@ def close_db(error):
     storage.close()
 
 """ homepage endpoint """
+
+
 @app.route('/')
 def home():
     """
@@ -32,9 +35,11 @@ def home():
     for user in users.values():
         users_list.append(user.to_dict())
     return render_template("index.html", recipes=recipes,
-                            user="")
+                           user="")
 
 """ recipe without id """
+
+
 @app.route('/recipe')
 def recipe():
     """
@@ -47,6 +52,8 @@ def recipe():
     return render_template("recipes.html", recipes=recipe_dict)
 
 """ endpoint recipe with id """
+
+
 @app.route('/recipe/<id>')
 def recipe_id(id):
     """
@@ -59,26 +66,29 @@ def recipe_id(id):
     return render_template("recipes.html", recipes=recipes)
 
 """ endpoint to create new common user """
+
+
 @app.route('/new_user', methods=['POST'])
 def new_user():
     """
         get data from form and do post request to add this to database
     """
     first_name = request.form["first_name"]
-    last_name=request.form["last_name"]
-    nickname=request.form["nickname"]
-    email=request.form["email"]
+    last_name = request.form["last_name"]
+    nickname = request.form["nickname"]
+    email = request.form["email"]
     password = request.form["password"]
     user_type = 0
     dict_user = {
-        'first_name':first_name,
-        'last_name':last_name,
-        'nickname':nickname,
-        'email':email,
-        'password':password,
-        'user_type':user_type
+        'first_name': first_name,
+        'last_name': last_name,
+        'nickname': nickname,
+        'email': email,
+        'password': password,
+        'user_type': user_type
     }
-    r = requests.post(url="http://0.0.0.0:5000/api/v1/new_user", json=dict_user)
+    r = requests.post(url="http://0.0.0.0:5000/api/v1/new_user",
+                      json=dict_user)
     if r.status_code == 400:
         print("Usuario no creado")
     else:
@@ -86,30 +96,33 @@ def new_user():
     return redirect("/")
 
 """ endpoint to create new chef user """
+
+
 @app.route('/new_chef', methods=['POST'])
 def new_chef():
     """
         get data from form and do post request to add this to database
     """
     first_name = request.form["first_name"]
-    last_name=request.form["last_name"]
-    nickname=request.form["nickname"]
-    email=request.form["email"]
+    last_name = request.form["last_name"]
+    nickname = request.form["nickname"]
+    email = request.form["email"]
     password = request.form["password"]
     website = request.form["website"]
     profile_image = request.form["profile_image"]
     user_type = 1
     dict_chef = {
-        'first_name':first_name,
-        'last_name':last_name,
-        'nickname':nickname,
-        'email':email,
-        'password':password,
-        'website':website,
-        'profile_image':profile_image,
-        'user_type':user_type
+        'first_name': first_name,
+        'last_name': last_name,
+        'nickname': nickname,
+        'email': email,
+        'password': password,
+        'website': website,
+        'profile_image': profile_image,
+        'user_type': user_type
     }
-    r = requests.post(url="http://0.0.0.0:5000/api/v1/new_user", json=dict_chef)
+    r = requests.post(url="http://0.0.0.0:5000/api/v1/new_user",
+                      json=dict_chef)
     if r.status_code == 400:
         print("Usuario no creado")
     else:
@@ -117,26 +130,29 @@ def new_chef():
     return redirect("/")
 
 """ endpoint to create new recipe """
+
+
 @app.route('/new_recipe', methods=['POST'])
 def new_recipe():
     """
         get data from form and do post request to add this to database
     """
     name = request.form["name"]
-    description=request.form["description"]
-    ingredients=request.form["ingredients"]
-    preparation=request.form["preparation"]
+    description = request.form["description"]
+    ingredients = request.form["ingredients"]
+    preparation = request.form["preparation"]
     video_url = request.form["video_url"]
     recipe_img = request.form["recipe_img"]
     dict_recipe = {
-        'name':name,
-        'description':description,
-        'ingredients':ingredients,
-        'preparation':preparation,
-        'video_url':video_url,
-        'recipe_img':recipe_img
+        'name': name,
+        'description': description,
+        'ingredients': ingredients,
+        'preparation': preparation,
+        'video_url': video_url,
+        'recipe_img': recipe_img
         }
-    r = requests.post(url="http://0.0.0.0:5000/api/v1/new_user", json=dict_recipe)
+    r = requests.post(url="http://0.0.0.0:5000/api/v1/new_user",
+                      json=dict_recipe)
     if r.status_code == 400:
         print("Receta no creada")
     else:
